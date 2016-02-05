ee6faa4b8f81f51d88db7e817b8d9a18e8191a9e
Amanda Baker <amandaabaker21@gmail.com>
2016-02-05 12:09:01 -0500

    Add project schema

---
+++ modules/ratings/server/models/projects.server.model.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var ProjectSchema = new Schema({
+  teamName: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+  /* Not sure how to store logo yet
+  ,
+  logo: {
+    type: ???
+  }
+  */
+});
+
+mongoose.model('Project', ProjectSchema);
