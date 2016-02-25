a82f5bc91788653e285c5bdb269f0e17ed9723ac
James Risberg <jimmyrisberg56@gmail.com>
2016-02-25 00:24:23 -0500

    Finish hooking things up

---
+++ modules/users/server/routes/admin.server.routes.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var adminPolicy = require('../policies/admin.server.policy'),
+  admin = require('../controllers/admin.server.controller'),
+  project = require('../../../projects/server/controllers/projects.server.controller'),
+  rating= require('../../../ratings/server/controllers/ratings.server.controller');
+
+module.exports = function (app) {
+  // User route registration first. Ref: #713
+  require('./users.server.routes.js')(app);
+
+  // Users collection routes
+  app.route('/api/users')
+    .get(adminPolicy.isAllowed, admin.listUser);
+
+  // Single user routes
+  app.route('/api/users/:userId')
+    .get(adminPolicy.isAllowed, admin.readUser)
+    .put(adminPolicy.isAllowed, admin.updateUser)
+    .delete(adminPolicy.isAllowed, admin.deleteUser);
+
+  /* TODO: add routes as follows:
+        app.route('/api/LOGICAL/PATH')
+          .get(adminPolicy.isAllowed, ADMIN/PROJECT/RATING.FUNCTION)
+          .put(adminPolicy.isAllowed, ADMIN/PROJECT/RATING.FUNCTION);
+  */
+
+  // Finish by binding the user middleware
+  app.param('userId', admin.userByID);
+  app.param('projectId', project.projectByID);
+};
