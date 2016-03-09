782b0a8240e07fdd46c67b7be0d2ec6a33a2f3be
Amanda Baker <amandaabaker21@gmail.com>
2016-03-09 16:27:33 -0500

    Update user schema to include an enum for 'judge'

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -79,7 +79,7 @@ var UserSchema = new Schema({
   roles: {
     type: [{
       type: String,
-      enum: ['user', 'admin']
+      enum: ['user', 'admin', 'judge']
     }],
     default: ['user'],
     required: 'Please provide at least one role'
@@ -177,7 +177,7 @@ UserSchema.statics.generateRandomPassphrase = function () {
     var password = '';
     var repeatingCharacters = new RegExp('(.)\\1{2,}', 'g');
 
-    // iterate until the we have a valid passphrase. 
+    // iterate until the we have a valid passphrase.
     // NOTE: Should rarely iterate more than once, but we need this to ensure no repeating characters are present.
     while (password.length < 20 || repeatingCharacters.test(password)) {
       // build the random password
