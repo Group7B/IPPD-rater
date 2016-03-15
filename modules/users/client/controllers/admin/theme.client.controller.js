6306adf819dcde1c9a78479b0b0189c742f54929
James Risberg <jimmyrisberg56@gmail.com>
2016-03-15 13:10:55 -0400

    Color picker works!

--- modules/users/client/controllers/admin/theme.client.controller.js
+++ modules/users/client/controllers/admin/theme.client.controller.js
@@ -2,37 +2,61 @@
 
 angular.module('users.admin').controller('ThemeController', ['$scope', 'Theme',
   function ($scope, Theme) {
-    
-    $scope.addCSSRule = function(sheet, selector, rules, index) {
+    console.log(document.styleSheets);
+
+    $scope.addCSSRule = function (sheet, selector, rules, index) {
       sheet.deleteRule(index);
-      if('insertRule' in sheet) {
+      if ('insertRule' in sheet) {
         sheet.insertRule(selector + '{' + rules + '}', index);
       } else if ('addRule' in sheet) {
         sheet.addRule(selector, rules, index);
       }
     };
-    
-    $scope.updateDatabase = function (backgroundColor, accentColor, textColor) {
-      var theme = new Theme({
-        ID: '0',
-        backgroundColor: backgroundColor,
-        accentColor: accentColor,
-        textColor: textColor
-      });
-      
-      theme.$update(function () {
-        console.log('theme updated');
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
+
+    $scope.updateDatabase = function () {
+      Theme.get(function (data) {
+        var theme = data;
+        
+        if ($scope.backgroundColor) {
+          theme.backgroundColor = $scope.backgroundColor;
+        }
+
+        if ($scope.accentColor) {
+          theme.accentColor = $scope.accentColor;
+        }
+
+        if ($scope.textColor) {
+          theme.textColor = $scope.textColor;
+        }
+
+        theme.$update(function () {
+          console.log('theme updated');
+        }, function (errorResponse) {
+          $scope.error = errorResponse.data.message;
+        });
       });
     };
-    
-    $scope.updateStoredColors = function(backgroundColor, accentColor, textColor) {
-      $scope.addCSSRule(document.styleSheets[4], '.backgroundColor', 'background-color: ' + backgroundColor, 0);
-      $scope.addCSSRule(document.styleSheets[4], '.accentColor', 'background-color: ' + accentColor, 1);
-      $scope.addCSSRule(document.styleSheets[4], '.textColor', 'color: ' + textColor, 2);
+
+    $scope.updateStoredColors = function () {
+      if ($scope.backgroundColor) {
+        $scope.addCSSRule(document.styleSheets[5], '.backgroundColor', 'background-color: ' + $scope.backgroundColor, 0);
+      }
       
-      $scope.updateDatabase(backgroundColor, accentColor, textColor);
+      if ($scope.accentColor) {
+        $scope.addCSSRule(document.styleSheets[5], '.accentColor', 'background-color: ' + $scope.accentColor, 1);
+      }
+      
+      if ($scope.textColor) {
+        $scope.addCSSRule(document.styleSheets[5], '.textColor', 'color: ' + $scope.textColor, 2);
+      }
+
+      $scope.updateDatabase();
+    };
+
+    $scope.clearFields = function () {
+      $scope.backgroundColor = null;
+      $scope.accentColor = null;
+      $scope.textColor = null;
     };
   }
 ]);
