a68d79cc6de46c89191a2e223adae589f53169f5
Amanda Baker <amandaabaker21@gmail.com>
2016-04-06 11:45:07 -0400

    Incomplete: Fill testing db with projects before ratings e2e tests

--- modules/ratings/tests/e2e/ratings.e2e.tests.js
+++ modules/ratings/tests/e2e/ratings.e2e.tests.js
@@ -1,6 +1,33 @@
 'use strict';
 
+// TODO: determine how to populate database with projects for testing
+/*require('modules/users/server/models/user.server.model.js');
+var mongoose = require('mongoose'),
+  User = mongoose.model('User'),
+  Project = mongoose.model('Project'),
+  Rating = mongoose.model('Rating');
+
+var initRatings = function() {
+  var project1 = new Project({
+    teamName: 'DJ Khaled',
+    description: '#BlessUp',
+    logo: 'default.jpg'
+  });
+
+  var project2 = new Project({
+    teamName: 'Ermm',
+    description: 'Seriously running out of ideas..',
+    logo: 'Ayyyy.jpg'
+  });
+
+  project1.save();
+
+  project2.save();
+};*/
+
 describe('Ratings E2E Tests:', function () {
+  //initRatings();
+
   describe('Test ratings page', function () {
     it('Should report missing credentials', function () {
       browser.get('http://localhost:3001/ratings');
