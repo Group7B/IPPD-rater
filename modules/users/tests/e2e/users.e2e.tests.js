1b0cd1bc8370d9bb627b5af5366fb6a9397ccfaf
Amanda Baker <amandaabaker21@gmail.com>
2016-04-19 23:15:15 -0400

    Add e2e tests to ensure that non-admins cannot access admin dashboard

--- modules/users/tests/e2e/users.e2e.tests.js
+++ modules/users/tests/e2e/users.e2e.tests.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var protractor = require('protractor');
 describe('Users E2E Tests:', function () {
   var user1 = {
     firstName: 'test',
@@ -327,116 +327,33 @@ describe('Users E2E Tests:', function () {
 
   });
 
-  /*describe ('Change Password Settings Validation', function () {
+  describe('Admin Validation', function () {
 
-    it('Should report missing passwords', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Errors
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('Your current password is required.');
-      expect(element.all(by.css('.error-text')).get(1).getText()).toBe('Enter a new password.');
-      expect(element.all(by.css('.error-text')).get(2).getText()).toBe('Verify your new password.');
+    it('Should not let a guest view the admin panel', function () {
+      //Make sure user is signed out first
+      signout();
+      // Get admin page
+      browser.get('http://localhost:3001/admin/users');
+      // Expect redirect
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/');
     });
 
-    it('Should report a password with less than 10 characters long - "P@$$w0rd!"', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter Invalid Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('P@$$w0rd!');
+    it('Should not let a non-admin user view the admin panel', function () {
+      //Make sure user is signed out first
+      signout();
+      //Sign in
+      browser.get('http://localhost:3001');
+      // Enter UserName
+      element(by.model('credentials.username')).sendKeys(user1.username);
+      // Enter Password
+      element(by.model('credentials.password')).sendKeys(user1.password);
       // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Error
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must be at least 10 characters long.');
+      element(by.css('button[type="submit"]')).click();
+      // Try to access admin panel
+      browser.get('http://localhost:3001/admin/users');
+      // Expect forbidden
+      expect(element.all(by.css('h1')).get(0).getText()).toBe('Forbidden');
     });
+  });
 
-    it('Should report a password with greater than 128 characters long.', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter Invalid Password
-      element(by.model('passwordDetails.newPassword')).sendKeys(')!/uLT="lh&:`6X!]|15o!$!TJf,.13l?vG].-j],lFPe/QhwN#{Z<[*1nX@n1^?WW-%_.*D)m$toB+N7z}kcN#B_d(f41h%w@0F!]igtSQ1gl~6sEV&r~}~1ub>If1c+');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Error
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must be fewer than 128 characters.');
-    });
-
-    it('Should report a password with more than 3 or more repeating characters - "P@$$w0rd!!!"', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter Invalid Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('P@$$w0rd!!!');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Error
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password may not contain sequences of three or more repeated characters.');
-    });
-
-    it('Should report a password with no uppercase letters - "p@$$w0rd!!"', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter Invalid Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('p@$$w0rd!!');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Error
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must contain at least one uppercase letter.');
-    });
-
-    it('Should report a password with less than one number - "P@$$word!!"', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter Invalid Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('P@$$word!!');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Error
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must contain at least one number.');
-    });
-
-    it('Should report a password with less than one special character - "Passw0rdss"', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter Invalid Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('Passw0rdss');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Error
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must contain at least one special character.');
-    });
-
-    it('Should report passwords do not match', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter New Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('P@$$w0rds!!');
-      // Verify New Password
-      element(by.model('passwordDetails.verifyPassword')).sendKeys(user1.password);
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Errors
-      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('Passwords do not match.');
-    });
-
-    it('Should change the password to - "P@$$w0rds!!"', function () {
-      browser.get('http://localhost:3001/settings/password');
-      // Enter Current Password
-      element(by.model('passwordDetails.currentPassword')).sendKeys(user1.password);
-      // Enter New Password
-      element(by.model('passwordDetails.newPassword')).sendKeys('P@$$w0rds!!');
-      // Verify New Password
-      element(by.model('passwordDetails.verifyPassword')).sendKeys('P@$$w0rds!!');
-      // Click Submit button
-      element(by.css('button[type=submit]')).click();
-      // Password Changed
-      expect(element.all(by.css('.text-success')).get(0).getText()).toBe('Password Changed Successfully');
-    });
-  });*/
 });
